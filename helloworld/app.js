1790ce5910bb6ebd18a0e9a5f822152c21ac9d7c
Per Christian Moen <pcmoen@hsnews.no>
2017-02-02 16:18:31 +0100

    Updated compose file to version 3. Using the official consul image. Changed consul templates for nginx so that proxy_pass forwards based on URI context path insteade virtual hosts.

--- helloworld/app.js
+++ helloworld/app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World! from ' + process.env.HOSTNAME);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
   console.log('Server started');
 });
